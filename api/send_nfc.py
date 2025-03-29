28a2d9d1bc1322b3d64c3bdeac48e0a5bec57274
Ale99itA <141236779+Ale99itA@users.noreply.github.com>
2025-03-29 18:42:55 +0100

    Update send_nfc.py

--- api/send_nfc.py
+++ api/send_nfc.py
@@ -2,14 +2,17 @@ import os
 import telebot
 from flask import Flask, request
 
-TOKEN = os.environ.get('TELEGRAM_TOKEN')
-CHAT_ID = os.environ.get('CHAT_ID')
-bot = telebot.TeleBot(TOKEN)
-
 app = Flask(__name__)
 
-@app.route('/send_nfc', methods=['GET'])
+# Recupera i dati dalle variabili d'ambiente
+TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHAT_ID = os.environ.get('CHAT_ID')
+
+bot = telebot.TeleBot(TOKEN)
+
+@app.route('/api/send_nfc', methods=['GET'])
 def send_nfc():
+    # Gestisci la richiesta e invia un messaggio tramite il bot
     user_agent = request.headers.get('User-Agent')
     bot.send_message(CHAT_ID, f"Un dispositivo ({user_agent}) ha scansionato il Tag NFC!")
     return "OK", 200
