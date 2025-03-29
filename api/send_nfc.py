ba747e32925bdb23e8a6ede98d5068836b6b56e4
Ale99itA <141236779+Ale99itA@users.noreply.github.com>
2025-03-29 19:59:57 +0100

    Update send_nfc.py

--- api/send_nfc.py
+++ api/send_nfc.py
@@ -12,9 +12,20 @@ bot = telebot.TeleBot(TOKEN)
 
 @app.route('/api/send_nfc', methods=['GET'])
 def send_nfc():
-    # Gestisci la richiesta e invia un messaggio tramite il bot
-    user_agent = request.headers.get('User-Agent')
-    bot.send_message(CHAT_ID, f"Un dispositivo ({user_agent}) ha scansionato il Tag NFC!")
+    # Ottieni il parametro 'device_id' dall'URL; se non presente, usa un valore di default
+    device_id = request.args.get('device_id', 'Dispositivo sconosciuto')
+    
+    # Ottieni la data e l'ora correnti
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Crea un messaggio personalizzato utilizzando il device_id
+    message = f"{device_id} è arrivato sano e salvo alle {current_time} <3"
+    
+    # Invia il messaggio tramite il bot Telegram
+    bot.send_message(CHAT_ID, message)
+    
+    # Rispondi "OK" alla richiesta (utile per il debug)
     return "OK", 200
 
 if __name__ == "__main__":
